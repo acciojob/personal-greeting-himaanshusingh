338871b1907265a88f1df745774c23892c6339b9
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-17 10:32:03 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./../styles/App.css";
-import Greeting from "./Greeting";
 
 const App = () => {
+  const [value, setValue] = React.useState("");
+  const [text, setText] = React.useState("");
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setValue(value);
+    if (value) setText(`Hello ${value}!`);
+    else setText("");
+  }
+
   return (
     <div>
-      <Greeting />
+      <h1>Enter your name:</h1>
+      <input type="text" onChange={handleChange} value={value} />
+      <p>{text}</p>
     </div>
   );
 };
